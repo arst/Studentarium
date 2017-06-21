fded05ae2b7183a2c89e95c3c919ae5764e47679
arst <artem.startsev@gmail.com>
2017-06-21 19:29:01 +0300

    News view corrected

--- classic/src/view/news/NewsMainView.js
+++ classic/src/view/news/NewsMainView.js
@@ -1,4 +1,4 @@
-Ext.define('Test.view.news.NewsMainView',{
+Ext.define('Test.view.news.NewsMainView', {
     extend: 'Ext.panel.Panel',
     layout: {
         type: 'border'
@@ -11,28 +11,52 @@ Ext.define('Test.view.news.NewsMainView',{
 
     controller: 'newsMainView',
     items: [
-       {
-           title: 'Todays news',
-           region: 'center',
-           collapsible: false,
-           layout: 'fit',
-           items: []
-       },
-       {
-           title: 'Top news',
-           region: 'east',
-           width: 175,
-           minWidth: 100,
-           maxWidth: 250,
-           items: [
-               {
-                   xtype: 'grid',
-                   store: {
-                       type: 'news'
-                   }
-               }
-           ]
-       }
+        {
+            title: 'Todays news',
+            region: 'center',
+            xtype: 'panel',
+            collapsible: false,
+            layout: 'fit',
+            html: '',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    var data = {
+                        news: [
+                            { Title: 'News 1', PublishedAt: new Date(), ShortDescription: 'This is a good news...', Rating: '1000' },
+                            { Title: 'News 2', PublishedAt: new Date(), ShortDescription: 'This is a good news...', Rating: '900' },
+                            { Title: 'News 3', PublishedAt: new Date(), ShortDescription: 'This is a good news...', Rating: '800' },
+                            { Title: 'News 4', PublishedAt: new Date(), ShortDescription: 'This is a good news...', Rating: '500' },
+                            { Title: 'News 5', PublishedAt: new Date(), ShortDescription: 'This is a good news...', Rating: '600' },
+                            { Title: 'News 6', PublishedAt: new Date(), ShortDescription: 'This is a good news...', Rating: '900' }
+                        ]
+                    };
+                    var tpl = new Ext.XTemplate(
+                        '<tpl for="news">',
+                        '<p>{Title}</p>',
+                        '<p>{ShortDescription}</p>',
+                        '</tpl>'
+                    );
+                    debugger;
+                    tpl.overwrite(me.body, data);
+                }
+            }
+        },
+        {
+            title: 'Top news',
+            region: 'east',
+            width: 175,
+            minWidth: 100,
+            maxWidth: 250,
+            items: [
+                {
+                    xtype: 'grid',
+                    store: {
+                        type: 'news'
+                    }
+                }
+            ]
+        }
 
     ]
 });
